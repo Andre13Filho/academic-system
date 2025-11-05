96732c7bfdd71948b588e88aae5827e30c0bfb6f
Otavio Augusto <otavio.augusto.rosa1@gmail.com>
2025-11-05 20:21:42 -0300

    Enhance Student class with course and transcript attributes
    Added attributes and methods for course and academic transcript management.

--- src/model/Student.java
+++ src/model/Student.java
@@ -2,6 +2,39 @@
 // Atributos: course, "transcript" ou histórico ( lista de AcademicTranscript)
 // Métodos: matricularEmCurso, visualizarHistorico, getters e setters
 
-public class Student {
+import java.util.List;
+import java.util.ArrayList;
+
+public class Student extends Person{
+    private List<AcademicTranscript> historico;
+    private String curso;
+    private int matricula;
+
+    public Student(String nome, String cpf, String datanascimento, String curso, int matricula){
+        super(nome, cpf, datanascimento);
+        this.curso = curso;
+        this.matricula = matricula;
+        this.historico = new ArrayList<>();
+    }
+
+    public String getCurso() {
+        return curso;
+    }
+    public void setCurso(String curso) {
+        this.curso = curso;
+    }
+    public int getMatricula() {
+        return matricula;
+    }
+    public void setMatricula(int matricula) {
+        this.matricula = matricula;
+    }
+    public List<AcademicTranscript> getHistorico() {
+        return historico;
+    }
+    public void setHistorico(List<AcademicTranscript> historico) {
+        this.historico = historico;
+    }
+    
   
 }
