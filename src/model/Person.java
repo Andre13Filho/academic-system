67c3f951199597ceb65697425d2466640bedc36e
Otavio Augusto <otavio.augusto.rosa1@gmail.com>
2025-11-05 12:33:50 -0300

    Add getters and setters for Person attributes

--- src/model/Person.java
+++ src/model/Person.java
@@ -3,5 +3,42 @@
 // Métodos comuns: getters e setters
 
 public class Person {
-  
+    //private String matricula;
+    private String nome;
+    private String cpf;
+    String datanascimento;
+
+    public String getnome(){
+        return nome;
+    }
+    public void setnome(String nome){
+        if (nome != null){
+            this.nome = nome;
+        }
+        else{
+            System.out.println("Nome inválido");
+        }
+    }
+    public String getcpf(){
+        return cpf;
+    }
+    public void setcpf(String cpf){
+        if (cpf != null && cpf.length() == 11){
+            this.cpf = cpf;
+        }
+        else{
+            System.out.println("CPF inválido");
+        }
+    }
+    public String getdatanascimento(){
+        return datanascimento;
+    }
+    public void setdatanascimento(String datanascimento){
+        if (datanascimento != null){
+            this.datanascimento = datanascimento;
+        }
+        else{
+            System.out.println("Data de nascimento inválida");
+        }
+    }
 }
