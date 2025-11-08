552f15011e12acfeac7d35c6726e5a31a0317225
Otavio Augusto <otavio.augusto.rosa1@gmail.com>
2025-11-08 14:28:05 -0300

    Update Teacher.java

--- src/model/Teacher.java
+++ src/model/Teacher.java
@@ -2,6 +2,28 @@
 // Atributos: departamento, disciplinasMinistradas (lista de Subject)
 // Métodos: atribuirNota(Student, Subject), getters e setters
 
-public class Teacher {
-  
+import java.util.List;
+
+public class Teacher extends Person{
+    private String departamento;
+    private List<Subject> disciplinasMinistradas;
+    private float notas;
+    public Teacher(String nome, String cpf, String datanascimento, String email, String departamento, List<Subject> disciplinasMinistradas){
+        super(nome, cpf, datanascimento, email);
+        this.departamento = departamento;
+        this.disciplinasMinistradas = disciplinasMinistradas;
+    }
+    public String getDepartamento() {
+        return departamento;
+    }
+    public void setDepartamento(String departamento) {
+        this.departamento = departamento;
+    }
+    public List<Subject> getDisciplinasMinistradas() {
+        return disciplinasMinistradas;
+    }
+    public void setDisciplinasMinistradas(List<Subject> disciplinasMinistradas) {
+        this.disciplinasMinistradas = disciplinasMinistradas;
+    }
+
 }
