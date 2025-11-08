172fa736952b45853537361f810a77266fc37ab3
Otavio Augusto <otavio.augusto.rosa1@gmail.com>
2025-11-08 14:28:59 -0300

    Enhance Person class with email attribute
    Added email attribute and corresponding getter/setter methods.

--- src/model/Person.java
+++ src/model/Person.java
@@ -1,17 +1,19 @@
 // Classe abstrata ou base para Student e Teacher (por Herança)
 // Atributos comuns: nome, cpf, email, dataNascimento, matricula
 // Métodos comuns: getters e setters
-
+package model;
 public class Person {
     //private String matricula;
     private String nome;
     private String cpf;
     String datanascimento;
+    protected String email;
 
-    public Person(String nome, String cpf, String datanascimento) {
+    public Person(String nome, String cpf, String datanascimento, String email){ 
         this.nome = nome;
         this.cpf = cpf;
         this.datanascimento = datanascimento;
+        this.email = email;
     }
 
     public String getnome(){
@@ -45,6 +47,17 @@ public class Person {
         }
         else{
             System.out.println("Data de nascimento inválida");
+        }
+    }
+    public String getemail(){
+        return email;
+    }
+    public void setemail(String email){
+        if (email != null && email.contains("@")){
+            this.email = email;
+        }
+        else{
+            System.out.println("Email inválido");
         }
     }
 }
